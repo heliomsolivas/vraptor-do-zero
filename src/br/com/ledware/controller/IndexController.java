573c0b55dd6b11d85a0cd1d7e9371df4162a4500
Hélio Marcondes <heliomsolivas@gmail.com>
2015-02-04 14:18:17 -0200

    Primeiro commit
    Primeiro commit

---
+++ src/br/com/ledware/controller/IndexController.java
@@ -0,0 +1,20 @@
+package br.com.ledware.controller;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+
+@Resource
+public class IndexController {
+	
+@Path("/")	
+public void index(){
+	
+}
+
+@Path("/teste")
+public void teste(){
+	
+}
+}
